49ec7a1099403e87e4472efedebf7b32cb7eeed5
Selorianre <134551860+Selorianre@users.noreply.github.com>
2026-09-24 16:31:35 +0200

    Create MainActivity.java

---
+++ app/src/main/java/fr/bibleproia/app/MainActivity.java
@@ -0,0 +1,53 @@
+package fr.bibleproia.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+        webView.setWebViewClient(
+            new WebViewClient()
+        );
+
+        webView.loadUrl(
+            "https://bibleproia.onrender.com"
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+
+            webView.goBack();
+
+        } else {
+
+            super.onBackPressed();
+        }
+    }
+}
